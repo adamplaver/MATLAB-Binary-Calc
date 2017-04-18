964b80fd97c337b7ff588289a3feceae356f4c47
Adam Plaver <adam.plaver@gmail.com>
2017-04-18 16:14:01 -0400

    Completed Calculator and Cleaned up
    The calculator now works for both functions and returns the correct numbers. Does not handle negative numbers though. I also removed the mountains of junk comments that GUIDE created.

--- calc.m
+++ calc.m
@@ -19,13 +19,7 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before calc is made visible.
 function calc_OpeningFcn(hObject, eventdata, handles, varargin)
-% This function has no output args, see OutputFcn.
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to calc (see VARARGIN)
 
 % Choose default command line output for calc
 handles.output = hObject;
@@ -33,18 +27,9 @@ handles.output = hObject;
 % Update handles structure
 guidata(hObject, handles);
 
-% --- Outputs from this function are returned to the command line.
 function varargout = calc_OutputFcn(hObject, eventdata, handles) 
-	% varargout  cell array for returning output args (see VARARGOUT);
-	% hObject    handle to figure
-	% eventdata  reserved - to be defined in a future version of MATLAB
-	% handles    structure with handles and user data (see GUIDATA)
-
 	% Get default command line output from handles structure
 	varargout{1} = handles.output;
-
-% set(handles.Tag, 'Type', data);
-% get(handles.Tag, 'Type');
 
 function numReg1_Callback(hObject, eventdata, handles)
 	if(strcmp(get(handles.numReg1,'String'), 'Register 1'))
@@ -53,136 +38,123 @@ function numReg1_Callback(hObject, eventdata, handles)
 		else
 			bitSize = get(handles.regSize,'value');
 			bitSize = (2^bitSize);
-			if(str2num(get(handles.numReg1,'string')) > (bitSize))
+			if(str2double(get(handles.numReg1,'string')) >= (2^bitSize))
 				h = errordlg('Number too large for register. Register size has been increased.', 'Overflow');
-				while(str2num(get(handles.numReg1,'string')) > (bitSize))
-					tempBitSize = get(handles.regSize,'value')+1;
-					set(handles.regSize,'value', tempBitSize);
-					bitSize = (2^(get(handles.regSize,'value')));
-				end
-				binary = reshape(decimalToBinaryVector(str2num(get(handles.numReg1,'string')), bitSize,'MSBFirst').',1,[]);
+				while(str2double(get(handles.numReg1,'string')) >= (2^bitSize))
+					set(handles.regSize,'value', get(handles.regSize,'value')+1);
+					bitSize = 2^(2^(get(handles.regSize,'value')));
+                end
+                bitSize = get(handles.regSize,'value');
+                bitSize = (2^bitSize);
+				binary = reshape(decimalToBinaryVector(str2double(get(handles.numReg1,'string')), bitSize,'MSBFirst').',1,[]);
 				set(handles.reg1, 'Data', binary);
-				binary = reshape(decimalToBinaryVector(str2num(get(handles.numReg1,'string')), bitSize,'MSBFirst').',1,[]);
-				set(handles.reg1, 'Data', binary);
-			elseif(isempty(str2num(get(handles.numReg1,'string'))))
+				binary = reshape(decimalToBinaryVector(str2double(get(handles.numReg2,'string')), bitSize,'MSBFirst').',1,[]);
+				set(handles.reg2, 'Data', binary);
+			elseif(isempty(str2double(get(handles.numReg1,'string'))))
 				h = errordlg('Please enter a number.', 'Non-Number');
 				set(handles.numReg1, 'String', '');
 			else
-				binary = reshape(decimalToBinaryVector(str2num(get(handles.numReg1,'string')), bitSize,'MSBFirst').',1,[]);
+				binary = reshape(decimalToBinaryVector(str2double(get(handles.numReg1,'string')), bitSize,'MSBFirst').',1,[]);
 				set(handles.reg1, 'Data', binary);
 			end
 	end
 		
-% --- Executes during object creation, after setting all properties.
 function numReg1_CreateFcn(hObject, eventdata, handles)
-	% hObject    handle to numReg1 (see GCBO)
-	% eventdata  reserved - to be defined in a future version of MATLAB
-	% handles    empty - handles not created until after all CreateFcns called
-
-	% Hint: edit controls usually have a white background on Windows.
-	%       See ISPC and COMPUTER.
 	if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
 			set(hObject,'BackgroundColor','white');
 	end
 
-% --- Executes on button press in addButton.
 function addButton_Callback(hObject, eventdata, handles)
-
-% --- Executes on button press in negButton.
+    if(get(handles.negButton,'value') == 1)
+        set(handles.negButton,'value',0);
+    end
+    
 function negButton_Callback(hObject, eventdata, handles)
+    if(get(handles.addButton,'value') == 1)
+        set(handles.addButton,'value',0);
+    end
 
-% --- Executes on selection change in regSize.
 function regSize_Callback(hObject, eventdata, handles)
 	bitSize = get(handles.regSize,'value');
 	bitSize = (2^bitSize);
 	%Sets first register to new value
-	binary = reshape(decimalToBinaryVector(str2num(get(handles.numReg1,'string')), bitSize,'MSBFirst').',1,[]);
+	binary = reshape(decimalToBinaryVector(str2double(get(handles.numReg1,'string')), bitSize,'MSBFirst').',1,[]);
 	set(handles.reg1, 'Data', binary);
 	%Sets second register
-	binary = reshape(decimalToBinaryVector(str2num(get(handles.numReg2,'string')), bitSize,'MSBFirst').',1,[]);
+	binary = reshape(decimalToBinaryVector(str2double(get(handles.numReg2,'string')), bitSize,'MSBFirst').',1,[]);
 	set(handles.reg2, 'Data', binary);
 
-% --- Executes during object creation, after setting all properties.
 function regSize_CreateFcn(hObject, eventdata, handles)
-	% hObject    handle to regSize (see GCBO)
-	% eventdata  reserved - to be defined in a future version of MATLAB
-	% handles    empty - handles not created until after all CreateFcns called
-
-	% Hint: listbox controls usually have a white background on Windows.
-	%       See ISPC and COMPUTER.
 	if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
 			set(hObject,'BackgroundColor','white');
 	end
 
 function numReg2_Callback(hObject, eventdata, handles)
-	if(strcmp(get(handles.numReg2,'String'), 'Register 1'))
+	if(strcmp(get(handles.numReg2,'String'), 'Register 2'))
 			fprintf('')
 			set(handles.numReg2, 'String', '');
 		else
 			bitSize = get(handles.regSize,'value');
 			bitSize = (2^bitSize);
-			if(str2num(get(handles.numReg2,'string')) > (bitSize))
+			if(str2double(get(handles.numReg2,'string')) >= (2^bitSize))
 				h = errordlg('Number too large for register. Register size has been increased.', 'Overflow');
-				while(str2num(get(handles.numReg2,'string')) > (bitSize))
-					tempBitSize = get(handles.regSize,'value')+1;
-					set(handles.regSize,'value', tempBitSize);
-					bitSize = (2^(get(handles.regSize,'value')));
-				end
-				binary = reshape(decimalToBinaryVector(str2num(get(handles.numReg2,'string')), bitSize,'MSBFirst').',1,[]);
-				set(handles.reg2, 'Data', binary);
-				binary = reshape(decimalToBinaryVector(str2num(get(handles.numReg1,'string')), bitSize,'MSBFirst').',1,[]);
+				while(str2double(get(handles.numReg2,'string')) >= (2^bitSize))
+					set(handles.regSize,'value', get(handles.regSize,'value')+1);
+					bitSize = 2^(2^(get(handles.regSize,'value')));
+                end
+                bitSize = get(handles.regSize,'value');
+                bitSize = (2^bitSize);
+				binary = reshape(decimalToBinaryVector(str2double(get(handles.numReg1,'string')), bitSize,'MSBFirst').',1,[]);
 				set(handles.reg1, 'Data', binary);
-			elseif(isempty(str2num(get(handles.numReg2,'string'))))
+				binary = reshape(decimalToBinaryVector(str2double(get(handles.numReg2,'string')), bitSize,'MSBFirst').',1,[]);
+				set(handles.reg2, 'Data', binary);
+			elseif(isempty(str2double(get(handles.numReg1,'string'))))
 				h = errordlg('Please enter a number.', 'Non-Number');
 				set(handles.numReg2, 'String', '');
 			else
-				binary = reshape(decimalToBinaryVector(str2num(get(handles.numReg2,'string')), bitSize,'MSBFirst').',1,[]);
+				binary = reshape(decimalToBinaryVector(str2double(get(handles.numReg2,'string')), bitSize,'MSBFirst').',1,[]);
 				set(handles.reg2, 'Data', binary);
 			end
 	end
 
-% --- Executes during object creation, after setting all properties.
 function numReg2_CreateFcn(hObject, eventdata, handles)
-	% hObject    handle to numReg2 (see GCBO)
-	% eventdata  reserved - to be defined in a future version of MATLAB
-	% handles    empty - handles not created until after all CreateFcns called
-
-	% Hint: edit controls usually have a white background on Windows.
-	%       See ISPC and COMPUTER.
 	if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
 			set(hObject,'BackgroundColor','white');
 	end
 
-% --- Executes on button press in equalButton.
 function equalButton_Callback(hObject, eventdata, handles)
+if(get(handles.addButton,'Value') == 1)
+    % Add
+    sum = str2double(get(handles.numReg1,'String')) + str2double(get(handles.numReg2,'String'));
+    bitSize = get(handles.regSize,'value');
+	bitSize = (2^bitSize);
+    binary = reshape(decimalToBinaryVector(sum, bitSize,'MSBFirst').',1,[]);
+	set(handles.reg3, 'Data', binary);
+    set(handles.numFinal, 'String',sum);
+else
+    diff = str2double(get(handles.numReg1,'String')) - str2double(get(handles.numReg2,'String'));
+    if(diff < 0)
+        % Oh no
+        h = errordlg('Negative Result. The absolute value of the difference is shown.','Negative exception');
+        diff = diff * -1;
+    end
+    bitSize = get(handles.regSize,'value');
+	bitSize = (2^bitSize);
+    binary = reshape(decimalToBinaryVector(diff, bitSize,'MSBFirst').',1,[]);
+	set(handles.reg3, 'Data', binary);
+    set(handles.numFinal, 'String',diff);
+end
 
-
-% --- Executes during object deletion, before destroying properties.
 function reg1_DeleteFcn(hObject, eventdata, handles)
-% hObject    handle to reg1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 
 
-% --- Executes during object creation, after setting all properties.
 function reg1_CreateFcn(hObject, eventdata, handles)
 		set(hObject,'Data',cell(0));
-% hObject    handle to reg1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
 
 
-% --- Executes during object creation, after setting all properties.
 function reg2_CreateFcn(hObject, eventdata, handles)
 		set(hObject,'Data',cell(0));
-% hObject    handle to reg2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
 
 
-% --- Executes during object creation, after setting all properties.
 function reg3_CreateFcn(hObject, eventdata, handles)
 		set(hObject,'Data',cell(0));
-% hObject    handle to reg3 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
